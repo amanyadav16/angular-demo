5910474e5452b37d229678912c63e8294015e7c9
amanyadav16 <amanyadav16011998@gmail.com>
2022-08-26 14:53:53 +0530

    testing new branch commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo';
-  sString:string='';
-  onInput(value:string){
-      this.sString=value;
-     console.log(this.sString);     
-  }
+export class AppComponent implements OnInit{
+
+    ngOnInit(): void {
+      
+    }    
 }
